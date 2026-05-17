78a49e5c35ab82328d3b5aba02231bf565b42185
sanjeet <skkarmasi420@gmail.com>
2026-05-17 23:21:06 +0530

    findIntersectionValues1956

--- ARRAY/src/binary.java
+++ ARRAY/src/binary.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class binary {
+    public static boolean bin( int []  arr ,int  n   )  {
+        int start =    0 ;
+        int end = arr.length ;
+        while(start< end ) {
+//            int mid  =  start+ end  / 2  ;
+            int mid  = start+(end - start) / 2 ;
+
+
+        }
+
+        return  true ;
+    }
+    public static void main(String[] args) {
+        int []arr = { 1,2,3,4,5,6} ;
+          boolean ans  =  bin(arr , 4 ) ;
+        System.out.println(ans );
+    }
 }
--- ARRAY/src/findIntersectionValues1956.java
+++ ARRAY/src/findIntersectionValues1956.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
+import java.util.Set;
 
 public class findIntersectionValues1956 {
+
+
+    public   static   int[] findIntersectionValues(int[] nums1, int[] nums2) {
+
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for (int val : nums1) {
+            set1.add(val);
+        }
+
+        for (int val : nums2) {
+            set2.add(val);
+        }
+
+        int count1 = 0;
+        int count2 = 0;
+
+        for (int val : nums1) {
+            if (set2.contains(val)) {
+                count1++;
+            }
+        }
+
+        for (int val : nums2) {
+            if (set1.contains(val)) {
+                count2++;
+            }
+        }
+
+        return new int[]{count1, count2};
+    }
+
+    public static void main(String[] args) {
+        int [] num1  = {2,3,2 } ;
+        int[]  nums2 =  {1,2} ;
+
+        int [] ans  =  findIntersectionValues(num1 , nums2  ) ;
+        for(int val : ans ) {
+            System.out.print  (val +"  ");
+        }
+
+    }
 }
